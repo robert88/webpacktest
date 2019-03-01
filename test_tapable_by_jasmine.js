168737564df82805c42c7cae13f6559845ff05c4
robert88 <841354162@qq.com>
2019-03-01 18:09:28 +0800

    Add files via upload

--- test_tapable_by_jasmine.js
+++ test_tapable_by_jasmine.js
@@ -1,5 +1,7 @@
 debugger
 const pt =require("path");
 const jasmine = pt.resolve(process.env.APPDATA,"npm/node_modules/jasmine/bin/jasmine");
-process.argv.push(pt.resolve(process.env.APPDATA,"npm/node_modules/webpack/node_modules/tapable/lib/__tests__/Hook.js"));
+// process.argv.push(pt.resolve(process.env.APPDATA,"npm/node_modules/webpack/node_modules/tapable/lib/__tests__/Hook.js"));
+// process.argv.push(pt.resolve(process.env.APPDATA,"npm/node_modules/webpack/node_modules/tapable/lib/__tests__/HookCodeFactory.js"));
+process.argv.push(pt.resolve(process.env.APPDATA,"npm/node_modules/webpack/node_modules/tapable/lib/__tests__/SyncHook.js"));
 require(jasmine);
